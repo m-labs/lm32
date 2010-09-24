2d25c42a05ea9e84520ac5f8636ac1ad2ef9db67
Michael Walle <michael@walle.cc>
2010-09-24 09:25:54 +0200

    add JTAG support for Xilinx Spartan-6 devices
    Use the BSCAN_SPARTAN6 slice to connect the design to the hardware JTAG
    port.
    
    Signed-off-by: Michael Walle <michael@walle.cc>

--- rtl/jtag_tap_spartan6.v
+++ rtl/jtag_tap_spartan6.v
@@ -1,6 +1,6 @@
 /*
  * LatticeMico32
- * JTAG Registers
+ * JTAG Test Access Port For Xilinx Sparan-6 Devices
  *
  * Copyright (C) 2010 Michael Walle
  * All rights reserved.
@@ -28,61 +28,35 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-module jtag_cores (
-    input [7:0] reg_d,
-    input [2:0] reg_addr_d,
-    output reg_update,
-    output [7:0] reg_q,
-    output [2:0] reg_addr_q,
-    output jtck,
-    output jrstn
+module jtag_tap(
+	output tck,
+	output tdi,
+	input tdo,
+	output shift,
+	output update,
+	output reset
 );
 
-wire tck;
-wire tdi;
-wire tdo;
-wire shift;
-wire update;
-wire reset;
+wire g_shift;
+wire g_update;
 
-jtag_tap jtag_tap (
-	.tck(tck),
-	.tdi(tdi),
-	.tdo(tdo),
-	.shift(shift),
-	.update(update),
-	.reset(reset)
+assign shift = g_shift & sel;
+assign update = g_update & sel;
+
+BSCAN_SPARTAN6 #(
+	.JTAG_CHAIN(1)
+) bscan (
+	.CAPTURE(),
+	.DRCK(tck),
+	.RESET(reset),
+	.RUNTEST(),
+	.SEL(sel),
+	.SHIFT(g_shift),
+	.TCK(),
+	.TDI(tdi),
+	.TMS(),
+	.UPDATE(g_update),
+	.TDO(tdo)
 );
-
-reg [10:0] jtag_shift;
-reg [10:0] jtag_latched;
-
-always @(posedge tck or posedge reset)
-begin
-	if(reset)
-		jtag_shift <= 11'b0;
-	else begin
-		if(shift)
-			jtag_shift <= {tdi, jtag_shift[10:1]};
-		else
-			jtag_shift <= {reg_d, reg_addr_d};
-	end
-end
-
-assign tdo = jtag_shift[0];
-
-always @(posedge reg_update or posedge reset)
-begin
-	if(reset)
-		jtag_latched <= 11'b0;
-	else
-		jtag_latched <= jtag_shift;
-end
-
-assign reg_update = update;
-assign reg_q = jtag_latched[10:3];
-assign reg_addr_q = jtag_latched[2:0];
-assign jtck = tck;
-assign jrstn = ~reset;
 
 endmodule
